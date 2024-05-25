b3c69521a4b654f21a18fc1dcaf6bccef090804e
emily7lim <70532210+emily7lim@users.noreply.github.com>
2024-05-25 13:32:05 +0000

    add forms

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { useState, React } from "react";
 
 function App() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center-form'>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your annual income:
+          <input 
+            type="number" 
+            name="annualincome" 
+            value={inputs.annualincome || ""} 
+            onChange={handleChange}
+            placeholder="Annual Income"
+          />
+        </label>
+        <label>
+          Enter your monthly expense:
+          <input 
+            type="number" 
+            name="expense" 
+            value={inputs.expense || ""} 
+            onChange={handleChange}
+            placeholder="Monthly Expense"
+          />
+        </label>
+        <label>
+          Enter your job type:
+          <input 
+            type="text" 
+            name="job" 
+            value={inputs.job || ""} 
+            onChange={handleChange}
+            placeholder="Job Type"
+          />
+        </label>
+        <label>
+          Enter your dependencies:
+          <input 
+            type="number" 
+            name="dependencies" 
+            value={inputs.dependencies || ""} 
+            onChange={handleChange}
+            placeholder="Dependencies"
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 export default App;
